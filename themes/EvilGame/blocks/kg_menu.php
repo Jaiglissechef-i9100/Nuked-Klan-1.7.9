f1d19d1e9823065c02ba81b7769375456a3d4fa3
Jaiglissechef-i9100 <yaggiland@hotmail.com>
2017-05-20 01:18:21 +0200

    Update index News

--- themes/EvilGame/blocks/kg_menu.php
+++ themes/EvilGame/blocks/kg_menu.php
@@ -1,14 +1,14 @@
 	<a href="index.php">Home</a>
 	<a href="index.php?file=News">News</a>
 	<a href="index.php?file=Calendar">Calendrier </a>
-	<a  href="index.php?file=Team">Teams</a>
-	<a  href="index.php?file=Wars">Matches</a>
+	<a href="index.php?file=Team">Teams</a>
+	<a href="index.php?file=Wars">Matches</a>
 	<a href="index.php?file=Sections">Articles</a>
 	<a href="index.php?file=Server">Serveur</a>
 	<a href="index.php?file=Forum">Forum</a>
 	<a href="index.php?file=Download">Téléchargements</a> 
 	<a href="index.php?file=Members">Membres</a>
 	<a href="index.php?file=Gallery">Gallerie</a>
-	<a  href="index.php?file=Links">Liens</a>
+	<a href="index.php?file=Links">Liens</a>
 	 
 	 
