d2f6e12b5e5ed39ce1e806ba387528e92dc4fbc9
Jaiglissechef-i9100 <yaggiland@hotmail.com>
2017-06-15 19:25:00 +0200

    Update block event calendar

--- Includes/blocks/block_event.php
+++ Includes/blocks/block_event.php
@@ -9,11 +9,168 @@
 // -------------------------------------------------------------------------//
 defined("INDEX_CHECK") or die ('You can\'t run this file alone.');
 
-global $nuked, $language;
+global $nuked, $language, $bgcolor1, $bgcolor2, $bgcolor3;
 translate('modules/Calendar/lang/' . $language . '.lang.php');
 
+?>
+<link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
+<style>
+#calendar *{
+	margin: 0;
+	padding: 0;
+}
+
+.clasp {
+	background: #aaaaaa;
+	border: 1px solid #999999;
+	border-radius: 4px;
+	box-shadow: 0 -1px 5px rgba(0,0,0,0.4), inset -1px 1px 2px white;
+	display: block;
+	height: 16px;
+	position: absolute;
+	top: -5px;
+	width: 4px;
+	z-index: 2000;
+}
+.clasp.hole-left {
+	left: 9px;
+}
+.clasp.hole-right {
+	right: 9px;
+}
+
+.hole {
+	background: #99ccff;
+	border-radius: 8px;
+	box-shadow: 0 -1px 0 black, inset 0 0 20px #878787;
+	display: block;
+	height: 8px;
+	position: absolute;
+	top: 8px;
+	width: 8px;
+}
+
+.hole-left {
+	left: 8px;
+}
+.hole-right {
+	right: 8px;
+}
+
+#calendar {
+	background: #e4e0e7;
+	box-shadow: 0 0 20px <?php echo $bgcolor3; ?>;
+	height: auto;
+	margin: 20px auto;
+	position: relative;
+	text-align: center;
+	width: 100%
+	
+}
+#eventscal {
+    height: auto;
+	margin: 20px auto;
+	position: relative;
+	text-align: center;
+	width: 100%
+}
+#eventscal .titreevents{
+    font-family: 'Poiret One', cursive;
+	/*color: <?php echo $bgcolor1; ?>;*/
+    background: <?php echo $bgcolor3; ?>; 
+    border-top: 1px <?php echo $bgcolor2; ?>;	
+	border-left: 1px <?php echo $bgcolor2; ?>;	
+	border-right: 1px <?php echo $bgcolor2; ?>;	
+	-webkit-border-top-left-radius: 5px;
+    -webkit-border-top-right-radius: 5px;
+    -moz-border-radius-topleft: 5px;
+    -moz-border-radius-topright: 5px;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+	padding: 5px;
+	
+}
+#month {
+	background: linear-gradient(<?php echo $bgcolor3; ?>, <?php echo $bgcolor2; ?>);
+	font-family: 'Poiret One', cursive;
+	font-size: 16px;
+	/*color: <?php echo $bgcolor1; ?>;*/
+	height: 30px;
+	line-height: 30px;
+	text-shadow: 0 -1px 0 <?php echo $bgcolor3; ?>;
+	width: 100%
+}
+#month a {
+    text-decoration:none;
+	/*color: <?php echo $bgcolor1; ?>;*/
+}
+#days {
+	background: linear-gradient(#f4f4f4, #ebebeb);
+	-webkit-box-shadow: 0 1px 0 #aaaaaa;
+}
+
+table.days {
+	color: #888888;
+	font-size: 14px;
+	width: 100%
+}
+
+table.day td, table.days td {
+	width: 47px;
+	
+}
+
+table.day td {
+	background: #f4f0f7;
+	color: #878787;
+	height: 29px;
+	box-shadow: 0 -1px 0 white;
+}
+
+table.day td.old-month {
+	color: #aaaaaa;
+	background: <?php echo $bgcolor1; ?>;
+}
+
+table.day td:hover {
+  background: #e9e9ee;
+  cursor: pointer;
+}
+
+table.day span.selected {
+	/*background: linear-gradient(<?php echo $bgcolor3; ?>, <?php echo $bgcolor1; ?>);*/
+	color: #C00000;
+	font-size: 14px;
+	text-shadow: 0 -1px 0 <?php echo $bgcolor2; ?>;
+	-webkit-border-radius: 90px;
+    -moz-border-radius: 90px;
+    border-radius: 90px;
+	padding:3px;
+}
+
+table.day td#appoint {
+	border-bottom: 3px solid #99ccff;
+}
+p#vtip { display: none; 
+position: absolute; 
+padding: 10px; 
+left: 5px; 
+font-size: 0.8em; 
+background-color: <?php echo $bgcolor2; ?>; 
+border: 1px solid <?php echo $bgcolor3; ?>; 
+-moz-border-radius: 5px; 
+-webkit-border-radius: 5px; 
+z-index: 9999 }
+
+p#vtip #vtipArrow { position: absolute; top: -10px; left: 5px }
+</style>
+<script type="text/javascript" src="Includes/blocks/js/vTip_v2/jquery.js"></script>
+<script type="text/javascript" src="Includes/blocks/js/vTip_v2/vtip.js"></script>
+<?php
 function affich_block_event($blok){
-    global $nuked, $bgcolor1, $bgcolor2, $bgcolor3, $file;
+// Patch calendar fr
+    global $nuked, $bgcolor1, $bgcolor2, $bgcolor3, $file, $language;
+// Fin patch calendar fr
 
     define ('ADAY', (61 * 60 * 24));
     $datearray = getdate();
@@ -56,38 +213,54 @@ function affich_block_event($blok){
         else $prevyear = $year;
 
     }
+// Patch calendar fr
+    if ($language == "french")
+	{
+		$start = mktime(0, 0, 0, $month, 1, $year);
+		$days = Array(_MON, _TUE, _WEN, _THR, _FRI, _SAT, _SUN);
+		$start2 = mktime(0, 0, 0, $month, 7, $year);
+		$firstdayarray = getdate($start2);
+	}
+	else	
+	{
+		$start = mktime(0, 0, 0, $month, 1, $year);
+		$days = Array(_SUN, _MON, _TUE, _WEN, _THR, _FRI, _SAT);
+		$firstdayarray = getdate($start);
+	}
+// Fin patch calendar fr	
 
-    $start = mktime(0, 0, 0, $month, 1, $year);
-    $firstdayarray = getdate($start);
+    //$start = mktime(0, 0, 0, $month, 1, $year);
+    //$firstdayarray = getdate($start);
 
     $months = Array(_JAN, _FEB, _MAR, _APR, _MAY, _JUN, _JUL, _AUG, _SEP, _OCT, _NOV, _DEC);
     $this_month = $month - 1;
-    $days = Array(_SUN, _MON, _TUE, _WEN, _THR, _FRI, _SAT);
+    //$days = Array(_SUN, _MON, _TUE, _WEN, _THR, _FRI, _SAT);
 
-    $blok['content'] .= '<table style="margin:0 auto;text-align:left" cellpadding="0" cellspacing="0"><tr><td>'."\n"
-					 . '<a href="index.php?file='.$file.'&amp;mo=' . $prevmonth . '&amp;ye='.$prevyear.'" title="'._PREVMONTH.'"><small>&lt;&lt;</small></a>&nbsp;<b>'.$months[$this_month].'&nbsp;'.$year.'</b>&nbsp;'."\n"
-					 . '<a href="index.php?file='.$file.'&amp;mo='.$nextmonth.'&amp;ye='.$nextyear.'" title="'._NEXTMONTH.'"><small>&gt;&gt;</small></a></td></tr></table>'."\n"
-					 . '<table style="margin:0 auto;text-align:left" cellpadding="2" cellspacing="1"><tr>'."\n";
+    $blok['content'] .= '<div id="calendar">
+	                     <div class="clasp hole-left"></div><div class="clasp hole-right"></div>
+	                     <div class="hole hole-left"></div><div class="hole hole-right"></div>
+	                     <div id="month"><a href="index.php?file='.$file.'&amp;mo=' . $prevmonth . '&amp;ye='.$prevyear.'" title="'._PREVMONTH.'">&laquo;&nbsp;</a>'.$months[$this_month].'&nbsp;'.$year.'<a href="index.php?file='.$file.'&amp;mo='.$nextmonth.'&amp;ye='.$nextyear.'" title="'._NEXTMONTH.'">&nbsp;&raquo;</a></div>'."\n"					 
+					  . '<div id="days"><table class="days"><tr>'."\n";
 
 	$size = count($days);
 	for($i=0; $i<$size; $i++){
-		$blok['content'] .= '<td style="text-align:center"><b>' . $days[$i] . '</b></td>';
+		$blok['content'] .= '<td><b>' . $days[$i] . '</b></td>';
 	}
 
     for($count = 0;$count < (6 * 7);$count++){
         $dayarray = getdate($start);
 
         if ((($count) % 7) == 0){
-            $blok['content'] .= '</tr><tr>';
+            $blok['content'] .= '</tr></table><table class="day"><tr>';
         }
 
         if ($count < $firstdayarray['wday'] || $dayarray['mon'] != $month){
-            $blok['content'] .= '<td>&nbsp;</td>';
+            $blok['content'] .= '<td class="old-month">&nbsp;</td>';
         }
         else{
             if ($dayarray['mday'] == $datearray['mday'] && $dayarray['mon'] == $datearray['mon']){
-                $bd = '<b>';
-                $bf = '</b>';
+                $bd = '<span class="selected"><b>';
+                $bf = '</b></span>';
             }
             else{
                 $bd = '';
@@ -164,9 +337,10 @@ function affich_block_event($blok){
                         }
                     }
 
-                    if ($heure1) $txt .= '<b>' . $heure1 . '</b><br />';
+                    if ($heure1) $txt .= '<br />&agrave;&nbsp;<b>' . $heure1 . '</b>&nbsp;';
                     $txt .= $titre1;
-                    $txt .= '<br />';
+                    //$txt .= '<br />';
+					$txt .= '';
 
                     $heure2 = $heure1;
                 }
@@ -189,11 +363,12 @@ function affich_block_event($blok){
                             $scores = '';
                         }
 
-                        if ($heure) $txt .= '<b>' . $heure . '</b><br />';
+                        if ($heure) $txt .= '<br />&agrave;&nbsp;<b>' . $heure . '</b>&nbsp;';
                         $txt .= _MATCH . '&nbsp;' . $type_match;
                         if ($adv_name) $txt .= '&nbsp;' . _VS . '&nbsp;' . $adv_name;
                         if ($scores)$txt .= '<br />' . $scores;
-                        $txt .= '<br /><br />';
+                        //$txt .= '<br /><br />';
+						$txt .= '';
                     }
                 }
 
@@ -225,13 +400,14 @@ function affich_block_event($blok){
 									$nom = $pseudo;
                                 }
 
-                                $txt .= '<b>' . _BIRTHDAY . ' : ' . $pseudo . '</b><br />' . _BIRTHDAYTEXT . '&nbsp;<b>' . $nom . '</b>&nbsp;' . _BIRTHDAYTEXTSUITE . '&nbsp;<b>' . $age . '</b>&nbsp;' . _YEARSOLD . '<br /><br />';
+                                $txt .= '<br />' . _BIRTHDAY . ' : <b>' . $pseudo . '</b><br />' . _BIRTHDAYTEXT . '&nbsp;<b>' . $nom . '</b>&nbsp;' . _BIRTHDAYTEXTSUITE . '&nbsp;<b>' . $age . '</b>&nbsp;' . _YEARSOLD . '';
                             }
                         }
                     }
                 }
 
-                $blok['content'] .= '<td style="background: ' . $bgcolor1 . ';border: 1px solid ' . $bgcolor3 . ';text-align:center;" onmouseover="AffBulle(\'&nbsp;&nbsp;&nbsp;&nbsp;' . $event_date . '&nbsp;' . $months[$this_month] . '&nbsp;' . $year . '\', \'' . htmlentities(mysql_real_escape_string($txt), ENT_NOQUOTES) . '\', 200)" onmouseout="HideBulle()">'."\n"
+                $blok['content'] .= '<td style"cursor:crosshair;" class="vtip" id="appoint" title="' . $event_date . '&nbsp;' . $months[$this_month] . '&nbsp;' . $year . '<br /> ' . $txt . '">'."\n"
+				
                 . '<a href="index.php?file=Calendar&amp;m=' . $month . '&amp;y=' . $year . '">'. $bd . $dayarray['mday'] . $bf . '</a></td>'."\n";
             }
             else{
@@ -242,8 +418,33 @@ function affich_block_event($blok){
         }
     }
 
-    $blok['content'] .= '</tr></table>'."\n";
-
+    $blok['content'] .= '</tr></table></div></div>'."\n";
+	
+	// listes événements 
+    $today = date("d");  	
+    $dbsEvents = '    SELECT titre, date_jour, date_mois, date_an, heure, auteur
+                        FROM '.CALENDAR_TABLE.'
+                        WHERE date_an = ' . $year . ' AND date_mois = ' . $month . ' AND '.$today.' < date_jour 
+						ORDER by date_jour
+                        ';
+    $dbeEvents = mysql_query($dbsEvents);
+    $nbEvents  = mysql_num_rows($dbeEvents); 
+	
+	if ($nbEvents != 0):
+   
+	
+    $blok['content'] .= '<div id="eventscal"><div class="titreevents">Ev&eacute;nements du mois</div>'."\n"
+	                 . "<table style=\"margin-left: auto;margin-right: auto;text-align: left;width:100%;\" border=\"0\" cellspacing=\"3\" cellpadding=\"3\">\n";
+	
+    while ($dataEvents  = mysql_fetch_assoc($dbeEvents)):
+	
+	$blok['content'] .= '<tr style="background: ' . $bgcolor1 . ';\"><td style="border: 1px dashed ' . $bgcolor3 . ';\">le '.$dataEvents['date_jour'].' : <i>'.$dataEvents['titre'].'</i></td></tr>'."\n";
+	
+    endwhile;   
+	
+	$blok['content'] .= '</table></div>'."\n";
+	                  //. 'le 11 &agrave; 16h: Salon de l\'auto'."\n";
+    endif;					  
     return $blok;
 }
 
@@ -268,9 +469,11 @@ function edit_block_event($bid){
 	   . '<tr><td><b>'._TITLE.'</b></td><td><b>'._BLOCK.'</b></td><td><b>'._POSITION.'</b></td><td><b>'._LEVEL.'</b></td></tr>'."\n"
 	   . '<tr><td style="text-align:center;" ><input type="text" name="titre" size="40" value="'.$titre.'" /></td>'."\n"
 	   . '<td style="text-align:center;"><select name="active">'."\n"
-	   . '<option value="1" '.$checked1.'>'._LEFT.'</option>'."\n"
-	   . '<option value="2" '.$checked2.'>'._RIGHT.'</option>'."\n"
-	   . '<option value="0" '.$checked0.'>'._OFF.'</option></select></td>'."\n"
+			, '<option value="1" ' , $checked1 , '>' , _LEFT , '</option>',"\n"
+			, '<option value="2" ' , $checked2 , '>' , _RIGHT , '</option>',"\n"
+			, '<option value="3" ' , $checked3 , '>' , _CENTERBLOCK , '</option>',"\n"
+			, '<option value="4" ' , $checked4 , '>' , _FOOTERBLOCK , '</option>',"\n"
+			, '<option value="0" ' , $checked0 , '>' , _OFF , '</option></select></td>',"\n"
 	   . '<td style="text-align:center;" ><input type="text" name="position" size="2" value="'.$position.'" /></td>'."\n"
 	   . '<td style="text-align:center;" ><select name="nivo"><option>'.$nivo.'</option>'."\n"
 	   . '<option>0</option>'."\n"
